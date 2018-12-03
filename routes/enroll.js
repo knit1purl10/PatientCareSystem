8d542d5408903375def760ff931b7b9f9dc610d6
Olivia Hon <olivia.hon@gmail.com>
2018-12-02 23:05:03 -0500

    Payment Controller
    Some page edits as well.

---
+++ routes/enroll.js
@@ -0,0 +1,26 @@
+var express=require("express");
+var connection = require('./../config');
+
+module.exports.enroll=function(req,res){
+    var users={
+        "user_name":req.body.username,
+        "user_type":req.body.type,
+        "password":req.body.password
+    }
+    connection.query('INSERT INTO User SET ?',users, function (error, results, fields) {
+      if (error) {
+        console.log(error);
+        res.json({
+            status:false,
+            message:'there are some error with query'
+        })
+      }else{
+          res.json({
+            status:true,
+            data:results,
+            message:'user registered sucessfully'
+        })
+        res.redirect('/public/login.html')
+      }
+    });
+}

34468ffd591481b0959a60016a8a8269fdcd9ca3
Olivia Hon <olivia.hon@gmail.com>
2018-12-04 23:21:17 -0500

    Bill and Payment History

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
   resave: false,
   saveUninitialized: false,
   cookie: {
-    expires: 600000
+    expires: 600000000000
   }
 }));
 
@@ -54,8 +54,8 @@ var sessionChecker = (req, res, next) => {
 
 app.route('/')
   .get(sessionChecker, (req, res) => {
-        res.sendFile(__dirname + '/public/login.html');
-    })
+    res.sendFile(__dirname + '/public/login.html');
+  })
 //
 
 app.get('/enroll', function(req, res) {
@@ -64,19 +64,19 @@ app.get('/enroll', function(req, res) {
 
 app.get('/dashboard', function(req, res) {
   if (req.session.user && req.cookies.user_sid) {
-        res.sendFile(__dirname + '/public/dashboard.html');
-    } else {
-        res.redirect('/login');
-    }
+    res.sendFile(__dirname + '/public/dashboard.html');
+  } else {
+    res.redirect('/login');
+  }
 })
 
 app.get('/logout', (req, res) => {
-    if (req.session.user && req.cookies.user_sid) {
-        res.clearCookie('user_sid');
-        res.redirect('/');
-    } else {
-        res.redirect('/');
-    }
+  if (req.session.user && req.cookies.user_sid) {
+    res.clearCookie('user_sid');
+    res.redirect('/');
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/payment', function(req, res) {
@@ -99,14 +99,22 @@ app.get('/insurance', function(req, res) {
   res.sendFile(__dirname + "/public/" + "insurance.html");
 })
 
+app.get('/history', function(req, res) {
+  res.sendFile(__dirname + "/public/" + "history.html");
+})
+
 //app.get('/viewSchedule', scheduleViewController.list);
 
 app.get("/viewSchedule", function(req, res, next) {
-   res.sendFile(__dirname + "/public/" + "viewSchedule.html");
+  res.sendFile(__dirname + "/public/" + "viewSchedule.html");
 });
 
 app.get("/patient", function(req, res, next) {
-   res.sendFile(__dirname + "/public/" + "patientRecord.html");
+  res.sendFile(__dirname + "/public/" + "patientRecord.html");
+});
+
+app.get("/paymentHistory", function(req, res, next) {
+  res.sendFile(__dirname + "/public/" + "paymentHistory.html");
 });
 
 app.get("/getSchedule", function(req, res) {
@@ -115,20 +123,20 @@ app.get("/getSchedule", function(req, res) {
 FROM patient_care_system.Visit as v \
 left join patient_care_system.Patient as p \
 on v.patient_id = p.user_id where ';
-if (req.session.userprofile == 'Doctor') {
+  if (req.session.userprofile == 'Doctor') {
     query += ' v.doctor_id = ? and '
     params.push(req.session.user_id)
-}
-query += 'yearweek(v.visit_start_time, 1) = yearweek(curdate(),1) \
+  }
+  query += 'yearweek(v.visit_start_time, 1) = yearweek(curdate(),1) \
 ORDER BY v.visit_start_time';
-   connection.query(query, params, function(err, rows, fs) {
-      if(err) {
-         console.log('Something is broken');
-         console.log(err);
-         console.log(fs);
-      }
-      res.json(rows);
-   });
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
+    }
+    res.json(rows);
+  });
 });
 
 app.get("/getPatientInfo", function(req, res) {
@@ -138,36 +146,36 @@ app.get("/getPatientInfo", function(req, res) {
   address_line1 as "Address 1", address_line2 as "Address 2", city as City,\
   state as State, zip_code as "Zip Code", phone_number as "Phone #" \
 FROM patient_care_system.Patient where ';
-if (req.session.userprofile == 'Patient') {
+  if (req.session.userprofile == 'Patient') {
     query += 'user_id = ?'
     params.push(req.session.user_id);
-} else {
-  if (req.query.pid != '' && req.query.search_name != '') {
-    query += 'user_id = ? and (lower(first_name) like ? or lower(last_name) like ?)'
-    params.push(req.query.pid);
-    params.push('%'+req.query.search_name+'%');
-    params.push('%'+req.query.search_name+'%');
-  } else{
-    if (req.query.pid != '') {
+  } else {
+    if (req.query.pid != '' && req.query.search_name != '') {
+      query += 'user_id = ? and (lower(first_name) like ? or lower(last_name) like ?)'
       params.push(req.query.pid);
-      query += 'user_id = ?'
+      params.push('%' + req.query.search_name + '%');
+      params.push('%' + req.query.search_name + '%');
     } else {
-      query += '(first_name like ? or last_name like ?)'
-      params.push('%'+req.query.search_name+'%');
-      params.push('%'+req.query.search_name+'%');
-    }
-  }
-  console.log(req.query.pid);
-  console.log(req.query.search_name);
-}
-   connection.query(query, params, function(err, rows, fs) {
-      if(err) {
-         console.log('Something is broken');
-         console.log(err);
-         console.log(fs);
+      if (req.query.pid != '') {
+        params.push(req.query.pid);
+        query += 'user_id = ?'
+      } else {
+        query += '(first_name like ? or last_name like ?)'
+        params.push('%' + req.query.search_name + '%');
+        params.push('%' + req.query.search_name + '%');
       }
-      res.json(rows);
-   });
+    }
+    console.log(req.query.pid);
+    console.log(req.query.search_name);
+  }
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
+    }
+    res.json(rows);
+  });
 });
 
 app.get("/getVisit", function(req, res) {
@@ -178,30 +186,30 @@ app.get("/getVisit", function(req, res) {
 FROM patient_care_system.Visit as v \
 left join patient_care_system.Patient as p \
 on v.patient_id = p.user_id \
-where v.visit_end_time is null \
-and (v.doctor_id = ? or v.doctor_id is null) ';
-if (req.session.userprofile == 'Doctor') {
+where  ';
+  if (req.session.userprofile == 'Doctor') {
+    query += 'and (v.doctor_id = ? or v.doctor_id is null) '
     params.push(req.session.user_id);
-    if (req.query.pid != '' && req.query.search_name != '') {
-      query += 'and p.user_id = ? and (lower(p.first_name) like ? or lower(p.last_name) like ?)'
-      params.push(req.query.pid);
-      params.push('%'+req.query.search_name+'%');
-      params.push('%'+req.query.search_name+'%');
-    } else{
-        query += 'and (lower(p.first_name) like ? or lower(p.last_name) like ?)'
-        params.push('%'+req.query.search_name+'%');
-        params.push('%'+req.query.search_name+'%');
+  }
+  if (req.query.pid != '' && req.query.search_name != '') {
+    query += ' p.user_id = ? and (lower(p.first_name) like ? or lower(p.last_name) like ?)'
+    params.push(req.query.pid);
+    params.push('%' + req.query.search_name + '%');
+    params.push('%' + req.query.search_name + '%');
+  } else {
+    query += ' (lower(p.first_name) like ? or lower(p.last_name) like ?) '
+    params.push('%' + req.query.search_name + '%');
+    params.push('%' + req.query.search_name + '%');
+  }
+  query += 'order by v.visit_start_time limit 1;';
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
     }
-    query += 'order by v.visit_start_time limit 1;';
-    connection.query(query, params, function(err, rows, fs) {
-       if(err) {
-          console.log('Something is broken');
-          console.log(err);
-          console.log(fs);
-       }
-       res.json(rows);
-    });
-}
+    res.json(rows);
+  });
 });
 
 app.post("/getTreatments", function(req, res) {
@@ -212,18 +220,87 @@ on t1.test_id = t2.trt_number \
 left join patient_care_system.Visit as v \
 on t1.visit_id = v.visit_id \
 where v.visit_id = ?;';
-if (req.session.userprofile == 'Doctor') {
+  params.push(req.body.vid);
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
+    }
+    console.log(rows);
+    return res.json(rows);
+  });
+});
+
+app.post("/closeVisit", function(req, res) {
+  var params = []
+  var query = 'update v set note = ?, visit_end_time = now(), visit_cost = t.total_bill \
+  from Visit as v inner join (select visit_id, sum(trt_cost) as total_bill \
+  from Treatment_Requested as t1 inner join Treatment as t2 \
+  on t1.test_id = t2.trt_number) t on t.visit_id = v.visit_id\
+              where visit_id = ?;';
+  if (req.session.userprofile == 'Doctor') {
+    console.log(req.body.notes);
+    console.log(req.body.vid);
+    params.push(req.body.notes);
     params.push(req.body.vid);
     connection.query(query, params, function(err, rows, fs) {
-       if(err) {
-          console.log('Something is broken');
-          console.log(err);
-          console.log(fs);
-       }
-       console.log(rows);
-       return res.json(rows);
+      if (err) {
+        console.log('Something is broken');
+        console.log(err);
+        console.log(fs);
+        res.json({
+          status: false,
+          message: 'there are some error with query'
+        })
+      }
+      console.log(rows);
+      return res.redirect('/patient');
     });
-}
+  }
+});
+
+app.get("/getPaymentHistory", function(req, res) {
+  var params = []
+  var query = 'select payment_id as "Bill ID", \
+  DATE_FORMAT(visit_start_time, "%m/%d/%Y") as Date, \
+  visit_cost as "Bill Amount", \
+  payment_method as "Payment Method", amount as "Amount" \
+FROM Payment inner join Visit \
+on Payment.visit_id = Visit.visit_id \
+where user_id = ? ORDER BY visit_start_time desc LIMIT 50';
+  console.log(req.session.user_id);
+  params.push(req.session.user_id);
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
+    }
+    res.json(rows);
+  });
+});
+
+app.get("/getHistory", function(req, res) {
+  var params = []
+  var query = 'select visit_id, doctor_id, \
+  date_format(visit_start_time, "%m/%d/%Y"), \
+  visit_cost \
+  from Visit \
+  inner join Doctor \
+  on Doctor.user_id = Visit.doctor_id \
+  where patient_id = ?\
+  and visit_end_time is not null; ORDER BY visit_start_time desc LIMIT 50';
+  console.log(req.session.user_id);
+  params.push(req.session.user_id);
+  connection.query(query, params, function(err, rows, fs) {
+    if (err) {
+      console.log('Something is broken');
+      console.log(err);
+      console.log(fs);
+    }
+    res.json(rows);
+  });
 });
 /* route to handle login and registration */
 app.post('/api/enroll', loginController.enroll);
